9e3f0008bf21716bd9746aaf366b0110d407b45c
suspiciousbanana <81795287+suspiciousbanana@users.noreply.github.com>
2021-07-26 16:42:45 -0700

    big navigation update

---
+++ navbarscroll.js
@@ -0,0 +1,13 @@
+/* Script that hides the navbar when scrolling down, and shows the navbar while scrolling up. */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbottom").style.bottom = "0";
+    document.getElementById("navtop").style.top = "0";
+  } else {
+    document.getElementById("navbottom").style.bottom = "-50px";
+    document.getElementById("navtop").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
